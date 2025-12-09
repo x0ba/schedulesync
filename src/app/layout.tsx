1603cbecbd1892f07383404f01daaa17887cc3e3
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-12-09 00:29:20 -0800

    identify users with posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { type Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { PostHogProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "ScheduleSync",
@@ -12,11 +13,31 @@ export const metadata: Metadata = {
     "A completely free tool to convert your course schedule into a calendar file you can import into your calendar app.",
   icons: [
     { rel: "icon", url: "/favicon/favicon.ico" },
-    { rel: "icon", url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    { rel: "icon", url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
     { rel: "apple-touch-icon", url: "/favicon/apple-touch-icon.png" },
-    { rel: "icon", url: "/favicon/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-    { rel: "icon", url: "/favicon/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    {
+      rel: "icon",
+      url: "/favicon/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
   ],
   manifest: "/favicon/site.webmanifest",
 };
@@ -31,11 +52,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${inter.variable}`}>
-        <body>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
-        </body>
-      </html>
+      <PostHogProvider>
+        <html lang="en" className={`${inter.variable}`}>
+          <body>
+            <TRPCReactProvider>{children}</TRPCReactProvider>
+          </body>
+        </html>
+      </PostHogProvider>
     </ClerkProvider>
   );
 }
--- src/env.js
+++ src/env.js
@@ -23,6 +23,8 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
   },
 
   /**
@@ -35,6 +37,8 @@ export const env = createEnv({
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
