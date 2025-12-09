0b4508230b4f2c634217dd43ef08ac872de50b9b
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-12-08 18:04:22 -0800

    add website title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ import { Inter } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
-  title: "",
+  title: "ScheduleSync",
   description:
-    "",
+    "A completely free tool to convert your course schedule into a calendar file you can import into your calendar app.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
